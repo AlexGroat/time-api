0ae2cfa6c8c099c56b71b7162b0848fa73713a51
Alex Groat <alex.groat@apeirogon.com.au>
2024-09-03 20:45:36 +0930

    setup route and controller scaffold for calculating complete weeks between date parameters.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
-use App\Http\Controllers\DayIntervalController;
+use App\Http\Controllers\Api\DayIntervalController;
+use App\Http\Controllers\CompleteWeekIntervalController;
+use App\Http\Controllers\WeekDayIntervalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/days-interval', DayIntervalController::class);
+
+Route::get('/weekdays-interval', WeekDayIntervalController::class);
+
+Route::get('/complete-weeks-interval', CompleteWeekIntervalController::class);
